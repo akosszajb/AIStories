50d4e4cd76c9a48a315f799992c31252bb99d82c
Akos Szajbert <kisszajbert@gmail.com>
2025-04-08 22:10:20 +0200

    Refactored Loading.jsx component

--- client/src/Components/Common/Loading/Loading.jsx
+++ client/src/Components/Common/Loading/Loading.jsx
@@ -1,5 +1,6 @@
 import "./Loading.css";
+import React from "react";
 
-const Loading = () => <div className="Loading" />;
+const Loading = () => <div className="Loading" data-testid="loading-spinner" />;
 
 export default Loading;
